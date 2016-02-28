96db80414b9b4135ddd37c9db32c63d6d5ffae67
tsk takezawa <tsk.take815@gmail.com>
2016-02-29 00:05:23 +0900

    Initial Commit

---
+++ Fastslack/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Fastslack
+//
+//  Created by to4iki on 2/28/16.
+//  Copyright © 2016 to4iki. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+}
