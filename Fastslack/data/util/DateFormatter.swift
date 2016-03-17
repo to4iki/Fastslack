9a30d9109530059e0d5cb9ce0576645e1491ae2e
tsk takezawa <tsk.take815@gmail.com>
2016-03-17 23:18:34 +0900

    Add DataFormatter/Logger utility

---
+++ Fastslack/data/util/DateFormatter.swift
@@ -0,0 +1,19 @@
+//
+//  DateFormatter.swift
+//  Fastslack
+//
+//  Created by to4iki on 3/17/16.
+//  Copyright © 2016 to4iki. All rights reserved.
+//
+
+import Foundation
+
+let dateFormatter: NSDateFormatter = {
+    let formatter = NSDateFormatter()
+    
+    formatter.timeStyle = .LongStyle
+    formatter.dateStyle = .ShortStyle
+    formatter.dateFormat = "yyyy/MM/dd HH:mm:ss"
+    
+    return formatter
+}()
