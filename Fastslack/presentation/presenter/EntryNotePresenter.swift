9556309d33477ad1291972b62019f56074c96b03
tsk takezawa <tsk.take815@gmail.com>
2016-03-19 00:15:56 +0900

    Make use of return observable

--- Fastslack/presentation/presenter/EntryNotePresenter.swift
+++ Fastslack/presentation/presenter/EntryNotePresenter.swift
@@ -7,52 +7,37 @@
 //
 
 import Foundation
+import RxSwift
 
 final class EntryNotePresenter {
     
+    private let disposeBag = DisposeBag()
+    
     private let entryUseCase = EntryNoteUseCase()
     
-    private let sendUseCase = SendNoteUseCase()
-    
-    func send(text: String) {
-        let note = entryUseCase.create(text)
-        sendUseCase.send(note)
-    }
+    private let sendUseCase = SendSlackUseCase()
 }
 
 // MARK: - Presenter
 
 extension EntryNotePresenter: Presenter {
-    
-    func viewDidLoad() {
-        entryUseCase.delegate = self
-        sendUseCase.delegate = self
-    }
 }
 
-// MARK: - EntryNoteUseCaseDelegate
+// MARK: - Action
 
-extension EntryNotePresenter: EntryNoteUseCaseDelegate {
+extension EntryNotePresenter {
     
-    func onSaveNoteSuccess() {
-        log.info("save note success.")
-    }
-    
-    func onSaveNoteError() {
-        log.error("save note error.")
-    }
-}
-
-// MARK: - SendNoteUseCaseDelegate
-
-extension EntryNotePresenter: SendNoteUseCaseDelegate {
-    
-    func onSendNoteSuccess(message: String, note: Note) {
-        log.info(message)
-        entryUseCase.save(note)
-    }
-    
-    func onSendNoteError(error: ErrorType) {
-        log.error("send note error.")
+    func send(message: String) {
+        let note = entryUseCase.create(message)
+        
+        sendUseCase.sendSlack(note.body)
+            .retry()
+            .flatMap { _ -> Observable<Note> in
+                self.entryUseCase.entry(note)
+            }
+            .subscribeCompleted {
+                log.info("complete send messsage")
+            }
+            .addDisposableTo(disposeBag)
     }
 }
