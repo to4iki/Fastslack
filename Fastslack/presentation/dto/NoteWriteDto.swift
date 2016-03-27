8d16c4ae86c601fce6073f5c5cba5f932b0c645e
tsk takezawa <tsk.take815@gmail.com>
2016-03-27 16:38:48 +0900

    Add NoteWriteDto

---
+++ Fastslack/presentation/dto/NoteWriteDto.swift
@@ -0,0 +1,37 @@
+//
+//  NoteWriteDto.swift
+//  Fastslack
+//
+//  Created by to4iki on 3/27/16.
+//  Copyright © 2016 to4iki. All rights reserved.
+//
+
+import Foundation
+import Slack
+
+struct NoteWriteDto {
+    
+    private let body: String
+}
+
+// MARK: - Converter
+
+extension NoteWriteDto {
+    
+    func convertNote(id: Int) -> Note {
+        let note = Note()
+        note.id = id
+        note.body = body
+        
+        return note
+    }
+    
+    func convertSlackMessage(attribute: MessageAttribute) -> Slack.Message {
+        return Slack.Message.build { (m: Slack.Message) in
+            m.channel(attribute.channel)
+            m.botName(attribute.botName)
+            m.iconEmoji(attribute.iconEmoji)
+            m.text(self.body, linkEnable: true)
+        }
+    }
+}
