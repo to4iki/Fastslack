c95e1ae77f68af49b76fa4901160d65cf145ca92
tsk takezawa <tsk.take815@gmail.com>
2016-04-02 20:59:53 +0900

    Add expired to Note

--- Fastslack/domain/model/Note.swift
+++ Fastslack/domain/model/Note.swift
@@ -10,25 +10,39 @@ import Foundation
 import RealmSwift
 
 final class Note: Object, Entity {
-    
-    dynamic var id: Int = 0
-    
-    dynamic var body: String = ""
-    
-    dynamic var createdAt: NSDate = NSDate()
+
+	dynamic var id: Int = 0
+
+	dynamic var body: String = ""
+
+	dynamic var createdAt: NSDate = NSDate()
+}
+
+// MARK: - Computed Properties
+
+extension Note {
+
+	private var exireDate: NSDate {
+		let beforeThirtyDaysTimeInterval: NSTimeInterval = -30 * 24 * 60 * 60
+		return NSDate(timeIntervalSinceNow: beforeThirtyDaysTimeInterval)
+	}
+
+	var expired: Bool {
+		return exireDate.compare(createdAt) == NSComparisonResult.OrderedDescending
+	}
 }
 
 // MARK: - Object
 
 extension Note {
-    
-    override static func primaryKey() -> String? {
-        return "id"
-    }
+
+	override static func primaryKey() -> String? {
+		return "id"
+	}
 }
 
 // MARK: - Equtable
 
 func == (lhs: Note, rhs: Note) -> Bool {
-    return lhs.id == rhs.id && lhs.body == rhs.body && lhs.createdAt == rhs.createdAt
+	return lhs.id == rhs.id && lhs.body == rhs.body && lhs.createdAt == rhs.createdAt
 }
